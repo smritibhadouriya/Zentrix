39a8e13f5baa4a58bd1a91fb9c4cb38e4f454c61
Tanya <tanyabhadouriya11@gmail.com>
2025-08-19 10:38:51 +0530

    Updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -96,7 +96,7 @@ const Footer = () => {
       type="email"
       name="email"
       placeholder="Enter your email"
-      className="flex-1 px-4 py-2 bg-gray-800 text-gray-300 border-l border-gray-700 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
+      className="flex-1 px-4 py-2 bg-gray-800 h-auto text-gray-300 border-l border-gray-700 rounded-l focus:outline-none focus:ring-2 focus:ring-blue-500"
       required
     />
     <button
